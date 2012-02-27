5445106477cf35dab03009a43f8fc2a1ffb661ae
smof <smof@Ubuntu-64.(none)>
2012-02-27 20:56:49 +0000

    initial commit

---
+++ merge_files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#File Merge and Appender 0.1
+#smof 20/02/12
+
+function merge() {
+	
+	mkdir orig/
+	mv *.* orig/
+	cat orig/*.* >> merged_file.dat
+	echo "Done merging...!"
+	exit
+}
+
+echo "This will merge all files in current directory, backing up origs.  Cont?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) merge;;
+        No ) exit;;
+    esac
+done
+
+
+
+
+
+
---
+++ multi2single.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#AWK port of my Java StringCAT util.  Concatenation tool for merging multi-line records to single-line
+#smof 27/02/12
+#Takes 2 args - first is orig file, 2nd is output file.  Assumes orig data contains only 2 fields.
+
+echo 'Starting...'
+
+#Checks number of arguments being passed is 2
+if [ $# -ne 2 ]
+ then
+	echo "Needs 2 args: ./stringCat.sh <orig_file> <output_file>"
+	exit 0
+fi
+
+#awk command embedded with time to show resources
+time awk '{a[$1]=a[$1]$2"|"}END{for(i in a)print i,a[i]}' $1 > $2
+
+echo 'Done!'
+
